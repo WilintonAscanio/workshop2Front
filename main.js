b4da5a1db66582e81bd9ea36b3420b930e3bb174
Esteark <eardila1409@gmail.com>
2023-01-27 18:20:20 -0500

    Add last changes esteban

--- main.js
+++ main.js
@@ -5,15 +5,34 @@ const nav__itemCompany = document.querySelector(".nav__itemCompany");
 const nav__iconMenu = document.querySelector(".nav__iconMenu");
 const btnCloseMenu = document.getElementById("btnCloseMenu");
 const nav__navigation = document.querySelector(".nav__navigation");
+const nav__ul = document.querySelector(".nav__ul");
 let menu = false;
 
+const showanimation = (objectHTML) => {
+  objectHTML.classList.remove("animate__fadeOutRight");
+  objectHTML.classList.add("animate__fadeInRight");
+};
+
+const hideanimation = (objectHTML) => {
+  objectHTML.classList.remove("animate__fadeInRight");
+  objectHTML.classList.add("animate__fadeOutRight");
+  setTimeout(() => {
+    objectHTML.style.display = "none";
+
+    console.log(objectHTML);
+  }, 1000);
+};
+
 btnCloseMenu.addEventListener("click", () => {
-  nav__navigation.style.display = "none";
+  hideanimation(nav__navigation);
+
   menu = false;
 });
 
 nav__iconMenu.addEventListener("click", () => {
   nav__navigation.style.display = "flex";
+  showanimation(nav__navigation);
+
   menu = true;
 });
 
@@ -23,7 +42,6 @@ btnfeatures.addEventListener("click", () => {
 
 window.addEventListener("resize", () => {
   if (screen.width >= 900) {
-    console.log("si claro");
     btnfeatures.addEventListener("mouseover", () => {
       nav__itemFeature.classList.toggle("hidden");
     });
